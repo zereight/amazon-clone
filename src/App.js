9715c1767f50d31c0a46bb63d5721d402ac60f44
zereight <zereight@zereightui-MacBookPro.local>
2020-08-07 00:15:27 +0900

    Header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <h1>Chekcout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <h1>Main page</h1>
+          </Route>
+        </Switch>
+        {/* main page */}
+        {/* checkout page */}
+        {/* login page */}
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+
+function Header() {
+  return (
+    <nav className="header">
+      {/* logo left */}
+      <Link to="/">
+        <img
+          className="header__logo"
+          src="https://images-na.ssl-images-amazon.com/images/I/31%2BDgxPWXtL.jpg"
+          alt="amazon"
+        />
+      </Link>
+      <div className="header__search">
+        {/* search box */}
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+
+      <div className="header__nav">
+        {/* 1st link */}
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Hello zereight</span>
+            <span className="header__optionLineTwo">Sign In</span>
+          </div>
+        </Link>
+        {/* 2st link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Returns</span>
+            <span className="header__optionLineTwo">& Orders</span>
+          </div>
+        </Link>
+        {/* 3st link */}
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__optionLineOne">Your</span>
+            <span className="header__optionLineTwo">Prime</span>
+          </div>
+        </Link>
+        {/* 4st link */}
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header_basketCount">0</span>
+          </div>
+        </Link>
+      </div>
+      {/* Basket icon with number */}
+    </nav>
+  );
+}
+
+export default Header;
